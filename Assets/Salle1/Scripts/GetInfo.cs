74e1065d6a42467920041425dee8cc8682193c2d
Nathan LE COURIC <nathan.le_couric@etu.uca.fr>
2025-02-10 17:02:47 +0100

    Usage formatage JSON

--- Assets/Salle1/Scripts/GetInfo.cs
+++ Assets/Salle1/Scripts/GetInfo.cs
@@ -2,8 +2,23 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+[System.Serializable]
+public class Informations{
+    public string salle;
+    public string oeuvre;
+    public bool correct;
+}
+
+[System.Serializable]
+public class MessageData{
+    public string message;
+    public Informations informations;
+}
+
 class GetInfo : MonoBehaviour {
 
+    
     [SerializeField]
     private string message;
     private string currentScene;
@@ -12,36 +27,27 @@ class GetInfo : MonoBehaviour {
     private PNJFollow pnj;
 
     void Start(){
+        // Initialisation des constantes;
         currentScene = SceneManager.GetActiveScene().name; 
         pnj = PNJ.GetComponent<PNJFollow>();
-        message = "Sortie;Les Noces de Cana;Cette immense peinture de Véronèse illustre un festin biblique où le Christ accomplit son premier miracle : transformer leau en vin. Je peux vous guider vers cette œuvre, qui se trouve dans la salle dédiée à la Renaissance.;";
-        string s = getSalle();
-        string o = getOeuvre();
-        string m = getMessage();
-        if(s == currentScene){
-            Debug.Log("Nous sommes dans " + currentScene + "On cherche l'oeuvre " + o);
-            pnj.Choose(o);
-        } else if ( s == "Sortie" && currentScene == "Salle de la Renaissance"){
-            pnj.Choose(s);
+
+        message = "{ \"message\": \"Voici les informations de l'œuvre\", \"informations\": { \"salle\": \"Renaissance\", \"oeuvre\": \"La Joconde\", \"correct\": true } }";
+
+        MessageData data = JsonUtility.FromJson<MessageData>(message);
+
+        if(data.informations.salle == currentScene){
+            Debug.Log("Nous sommes dans " + currentScene + "On cherche l'oeuvre " + data.informations.oeuvre);
+            pnj.Choose(data.informations.oeuvre);
+        } else if ( data.informations.salle == "Sortie" && currentScene == "Renaissance"){
+            pnj.Choose(data.informations.salle);
         } else {
             Debug.Log("Nous ne sommes pas dans la bonne salle. ");
         }
 
     }
 
-    public string getSalle(){
-        return message.Split(';')[0];
-    }
-
-    public string getOeuvre(){
-        return message.Split(';')[1];
-    }
-
-    public string getMessage(){
-        return message.Split(';')[2];
-    }
-
     void Update(){
     }
 
 }
+

6c6431eb9d716efb347dccea81b1e082d26dd702
Lucas Chalmandrier <luchalmand@local.isima.fr>
2025-02-19 15:55:05 +0100

    Amelioration parametre gameplay

--- Assets/Scripts/GameplaySettingsManager.cs
+++ Assets/Scripts/GameplaySettingsManager.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 using TMPro;
+using UnityEngine.XR;
 
 public class GameplaySettingsManager : MonoBehaviour
 {
@@ -13,94 +12,78 @@ public class GameplaySettingsManager : MonoBehaviour
     [Header("Réglage de la Hauteur (Mode Assis)")]
     public Slider heightSlider;
     public TMP_Text heightValueText;
-    public Transform playerCamera;
+    public Transform cameraOffset;
 
-    [Header("Réduction de Nausée")]
-    // public Slider nauseaReductionSlider;
-    // public TMP_Text nauseaReductionValueText;
-    // public Volume postProcessingVolume; // Volume de post-processing qui contient le Vignette Effect
-
-    private Vignette vignetteEffect;
+    private bool isSittingMode = false;
 
     private void Start()
     {
-        LoadSettings();
+        // LoadSettings();
 
         standingModeToggle.onValueChanged.AddListener(delegate { UpdateModeSettings(); });
         sittingModeToggle.onValueChanged.AddListener(delegate { UpdateModeSettings(); });
 
         heightSlider.onValueChanged.AddListener(delegate { UpdateHeight(); });
-        // nauseaReductionSlider.onValueChanged.AddListener(delegate { UpdateNauseaReduction(); });
 
-        // if (postProcessingVolume.profile.TryGet(out vignetteEffect))
-        // {
-        //     UpdateNauseaReduction();
-        // }
-        // else
-        // {
-        //     Debug.LogWarning("L'effet de vignette (réduction de nausée) n'est pas trouvé dans le post-processing.");
-        // }
+        UpdateModeSettings();
     }
 
     private void UpdateModeSettings()
     {
-        bool isSittingMode = sittingModeToggle.isOn;
+        isSittingMode = sittingModeToggle.isOn;
         heightSlider.interactable = isSittingMode;
 
-        if (!isSittingMode) 
+        if (isSittingMode)
         {
-            heightSlider.value = 1.75f;
-            ApplyHeight(); 
+            ApplyHeight();
+        }
+        else
+        {
+            ResetToStandingMode();
         }
 
-        PlayerPrefs.SetInt("SittingMode", isSittingMode ? 1 : 0);
-        PlayerPrefs.Save();
+        // PlayerPrefs.SetInt("SittingMode", isSittingMode ? 1 : 0);
+        // PlayerPrefs.Save();
     }
 
     private void UpdateHeight()
     {
         heightValueText.text = heightSlider.value.ToString("F2") + "m";
-        ApplyHeight();
-        PlayerPrefs.SetFloat("PlayerHeight", heightSlider.value);
-        PlayerPrefs.Save();
+
+        if (isSittingMode)
+        {
+            ApplyHeight();
+            // PlayerPrefs.SetFloat("PlayerHeight", heightSlider.value);
+            // PlayerPrefs.Save();
+        }
     }
 
     private void ApplyHeight()
     {
-        if (playerCamera != null)
+        if (cameraOffset != null)
         {
-            playerCamera.localPosition = new Vector3(playerCamera.localPosition.x, heightSlider.value, playerCamera.localPosition.z);
+            cameraOffset.localPosition = new Vector3(cameraOffset.localPosition.x, heightSlider.value, cameraOffset.localPosition.z);
         }
     }
 
-    // private void UpdateNauseaReduction()
-    // {
-    //     nauseaReductionValueText.text = (nauseaReductionSlider.value * 100).ToString("F0") + "%";
-        
-    //     if (vignetteEffect != null)
-    //     {
-    //         vignetteEffect.intensity.value = nauseaReductionSlider.value;
-    //     }
-
-    //     PlayerPrefs.SetFloat("NauseaReduction", nauseaReductionSlider.value);
-    //     PlayerPrefs.Save();
-    // }
-
-    private void LoadSettings()
+    private void ResetToStandingMode()
     {
-        bool isSittingMode = PlayerPrefs.GetInt("SittingMode", 0) == 1;
-        sittingModeToggle.isOn = isSittingMode;
-        standingModeToggle.isOn = !isSittingMode;
-
-        float savedHeight = PlayerPrefs.GetFloat("PlayerHeight", 1.75f);
-        heightSlider.value = savedHeight;
-        heightValueText.text = savedHeight.ToString("F2") + "m";
-
-        // float savedNauseaReduction = PlayerPrefs.GetFloat("NauseaReduction", 0.5f);
-        // nauseaReductionSlider.value = savedNauseaReduction;
-        // nauseaReductionValueText.text = (savedNauseaReduction * 100).ToString("F0") + "%";
-
-        ApplyHeight();
-        // UpdateNauseaReduction();
+        if (cameraOffset != null)
+        {
+            cameraOffset.localPosition = new Vector3(cameraOffset.localPosition.x, 0f, cameraOffset.localPosition.z);
+        }
     }
+
+    // private void LoadSettings()
+    // {
+    //     isSittingMode = PlayerPrefs.GetInt("SittingMode", 0) == 1;
+    //     sittingModeToggle.isOn = isSittingMode;
+    //     standingModeToggle.isOn = !isSittingMode;
+
+    //     float savedHeight = PlayerPrefs.GetFloat("PlayerHeight", 1.3f);
+    //     heightSlider.value = savedHeight;
+    //     heightValueText.text = savedHeight.ToString("F2") + "m";
+
+    //     UpdateModeSettings();
+    // }
 }
